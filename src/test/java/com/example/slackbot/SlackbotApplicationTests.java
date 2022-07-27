ff7f3da5352d81b70f57e108ced85639b8ccd7a3
tae2089 <tae2089002@gmail.com>
2022-07-27 17:58:49 +0900

    update

---
+++ src/test/java/com/example/slackbot/SlackbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slackbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
